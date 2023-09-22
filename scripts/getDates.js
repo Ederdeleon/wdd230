0b82f3662a30117974aaf3d4fb2e8305f7a77fed
Ederdeleon <122401764+Ederdeleon@users.noreply.github.com>
2023-09-22 10:20:07 -0600

    final home page

---
+++ scripts/getDates.js
@@ -0,0 +1,9 @@
+// Get the last modified date/time of the current document
+var lastModifiedDate = document.lastModified;
+    
+// Display the last modified date/time
+document.getElementById("lastModified").textContent = lastModifiedDate;
+
+
+document.getElementById("year").textContent = new Date().getFullYear();
+
