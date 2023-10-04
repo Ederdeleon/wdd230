afd5d4b2e6910a1fda85a8995bb594f89ca02508
Ederdeleon <122401764+Ederdeleon@users.noreply.github.com>
2023-10-04 16:54:49 -0600

    commit

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -20,3 +20,19 @@ window.addEventListener('resize', () => {
         menu.classList.remove('show');
     }
 });
+
+// dark mode
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});
