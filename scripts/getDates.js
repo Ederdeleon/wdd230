c4b8bebb8e77cadb6795e5ed2829fe5c9f92feeb
Ederdeleon <122401764+Ederdeleon@users.noreply.github.com>
2023-10-04 16:20:29 -0600

    menu

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -7,3 +7,16 @@ document.getElementById("lastModified").textContent = lastModifiedDate;
 
 document.getElementById("year").textContent = new Date().getFullYear();
 
+// hamburger menu
+const hamburgerButton = document.querySelector('.hamburger-button');
+const menu = document.querySelector('.menu');
+
+hamburgerButton.addEventListener('click', () => {
+    menu.classList.toggle('show');
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+        menu.classList.remove('show');
+    }
+});
