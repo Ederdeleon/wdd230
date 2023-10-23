59609b15cce1725c7372f9b9a37d634ed920c3d0
Ederdeleon <122401764+Ederdeleon@users.noreply.github.com>
2023-10-23 16:41:46 -0600

    images-style

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -11,3 +11,27 @@ window.addEventListener('resize', () => {
         menu.classList.remove('show');
     }
 });
+
+// visits
+let visitelement=document.getElementById("lastvisit")
+if (!localStorage.getItem("visits")){
+ visitelement.innerHTML="Welcome! Let us know if you have any questions."
+localStorage.setItem("visits",Date.now())
+}
+else if (localStorage.getItem("visits")){
+    let lastvisit=localStorage.getItem("visits")
+        let now=Date.now()
+        let difference=now-lastvisit
+        let days=difference/1000/60/60/24
+        if (days<1){
+            visitelement.innerHTML="Back so soon! Awesome!"
+            localStorage.setItem("visits",Date.now())
+        }
+        else {
+            visitelement.innerHTML=`You last visited ${days} days ago.`
+            localStorage.setItem("visits",Date.now())
+        }
+    }
+
+
+
