6ed29cd2b7c21da04a0539005c0dc83a306b6c6f
Ederdeleon <122401764+Ederdeleon@users.noreply.github.com>
2023-11-13 15:38:50 -0700

    prophest

--- lesson5/scripts/prophets.js
+++ lesson5/scripts/prophets.js
@@ -4,13 +4,7 @@ const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophet
 // Declare a const variable name "cards" that selects the HTML div element from the document with an id value of "cards".
 const cards = document.querySelector('#cards');
 
-async function getProphetData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.table(data.prophets); // temporary testing of data retreival
-  }
-  
-  getProphetData();
+
 
 
   async function getProphetData(url) {
@@ -20,15 +14,38 @@ async function getProphetData() {
     displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
   }
   
-  getProphetData();
+  getProphetData(url);
 
 
-  const displayProphets = (prophets) => {
-    // card build code goes here
-  }
+  
 
   const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
+  card = document.createElement("section")
+  fullname = document.createElement("h2")
+  portrait = document.createElement("img")
+  birthplace = document.createElement("p")
+  birthdate = document.createElement("p")
   
+  console.log(prophet)
+  fullname.innerHTML = `${prophet.name} ${prophet.lastname}`
+  birthplace.innerHTML = `${prophet.birthplace}`
+  birthdate.innerHTML = `${prophet.birthdate}`
+
+  portrait.setAttribute("src",prophet.imageurl)
+  portrait.setAttribute("alt",`Portrait of ${prophet.name} ${prophet.lastname}`);
+  portrait.setAttribute('loading', 'lazy');
+  portrait.setAttribute('width', '340');
+  portrait.setAttribute('height', '440');
+
+
+  card.appendChild(fullname);
+  card.appendChild(birthplace);
+  card.appendChild(birthdate);
+  card.appendChild(portrait);
+  cards.appendChild(card)
+
     });
+
   }
+
