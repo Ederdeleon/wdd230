b253016035957f100bf62e7ab78875355ee4adfb
Ederdeleon <122401764+Ederdeleon@users.noreply.github.com>
2023-11-13 15:58:00 -0700

    jj

--- lesson5/scripts/weather.js
+++ lesson5/scripts/weather.js
@@ -3,4 +3,19 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat'
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=87bb23583624ad10081b33f2619bef62'
+
+async function apiFetch(){
+    try {
+        const response =  await fetch(url)
+       
+        if (response.ok){
+            const data = await response.json()
+            console.log (data)
+            throw response.text
+        }
+    } catch(error){
+        console.log (error)
+    }
+}
+apiFetch()
