b191808b571050c9e1d8a9e666c1d99c736a55e6
Ederdeleon <122401764+Ederdeleon@users.noreply.github.com>
2023-11-15 13:53:53 -0700

    links

--- scripts/links.js
+++ scripts/links.js
@@ -1,6 +1,6 @@
-const baseURL = "https://yourgithubusername.github.io/wdd230/";
+const baseURL = "https://Ederdeleon.github.io/wdd230/";
 
-const linksURL = "https://yourgithubusername.github.io/wdd230/data/links.json";
+const linksURL = "https://Ederdeleon.github.io/wdd230/data/links.json";
 
 async function getLinks() {
     const response = await fetch(linksURL);
