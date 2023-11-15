0d4492a8f560841348bbfd54d83d391a067c522f
Ederdeleon <122401764+Ederdeleon@users.noreply.github.com>
2023-11-15 10:19:43 -0700

    links

--- lesson5/scripts/weather.js
+++ lesson5/scripts/weather.js
@@ -3,19 +3,30 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=87bb23583624ad10081b33f2619bef62'
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=87bb23583624ad10081b33f2619bef62';
 
 async function apiFetch(){
     try {
-        const response =  await fetch(url)
+        const response =  await fetch(url);
        
         if (response.ok){
-            const data = await response.json()
-            console.log (data)
-            throw response.text
+            const data = await response.json();
+            console.log (data);
+        }else{
+            throw Error(await response.text());
         }
     } catch(error){
-        console.log (error)
+        console.log (error);
     }
 }
-apiFetch()
+apiFetch();
+
+// display results
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.-----}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${----}.---`;
+    let desc = data.weather[0].-----;
+    weatherIcon.setAttribute(`---`, -----);
+    weatherIcon.setAttribute(`---`, -----);
+    captionDesc.textContent = `${desc}`;
+}
