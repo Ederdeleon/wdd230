dd3d3d24120bccb2288d7382b5319cb782516867
Ederdeleon <122401764+Ederdeleon@users.noreply.github.com>
2023-11-27 13:32:17 -0700

    homework

--- scripts/links.js
+++ scripts/links.js
@@ -1,3 +1,5 @@
+const links = document.getElementById ("links")
+
 const baseURL = "https://Ederdeleon.github.io/wdd230/";
 
 const linksURL = "https://Ederdeleon.github.io/wdd230/data/links.json";
@@ -12,5 +14,45 @@ async function getLinks() {
   getLinks();
 
 function displayLinks(weeks){
+ weeks.weeks.forEach(week => {
+   console.log(week)
+  
+   const listItem = document.createElement("li")
+   const header = document.createElement("h3")
+   header.innerHTML= week.week
+   listItem.appendChild (header)
+   week.links.forEach(link => {
+   
+  const url = document.createElement ("a")
+url.setAttribute("href", link.url)
+url.innerHTML=link.title
+listItem.appendChild(url)
+   })
 
-}
+links.appendChild (listItem)
+
+ });
+  
+ 
+  
+  // fullname.innerHTML = `${prophet.name} ${prophet.lastname}`
+  // birthplace.innerHTML = `${prophet.birthplace}`
+  // birthdate.innerHTML = `${prophet.birthdate}`
+
+  // portrait.setAttribute("src",prophet.imageurl)
+  // portrait.setAttribute("alt",`Portrait of ${prophet.name} ${prophet.lastname}`);
+  // portrait.setAttribute('loading', 'lazy');
+  // portrait.setAttribute('width', '340');
+  // portrait.setAttribute('height', '440');
+
+
+  // card.appendChild(fullname);
+  // card.appendChild(birthplace);
+  // card.appendChild(birthdate);
+  // card.appendChild(portrait);
+  // cards.appendChild(card)
+
+    ;
+
+  }
+
