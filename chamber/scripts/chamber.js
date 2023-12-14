3203212b6b4c446738ae27c299b1d17f87345c66
Ederdeleon <122401764+Ederdeleon@users.noreply.github.com>
2023-12-13 23:38:37 -0700

    w

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -35,40 +35,5 @@ else if (localStorage.getItem("visits")){
 
 // Weather code
 
-// select HTML elements in the document
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=40.52&lon=-111.86&units=imperial&appid=87bb23583624ad10081b33f2619bef62';
-
-async function apiFetch(){
-    try {
-        const response =  await fetch(url);
-       
-        if (response.ok){
-            const data = await response.json();
-            console.log (data);
-            displayResults(data);
-        }else{
-            throw Error(await response.text());
-        }
-        
-    } catch(error){
-        console.log (error);
-    }
-     
-}
-apiFetch();
-
-// display results
-function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    let desc = data.weather[0].description;
-    weatherIcon.setAttribute(`src`, iconsrc);
-    weatherIcon.setAttribute(`alt`, desc);
-    captionDesc.textContent = `${desc}`;
-    console.log(data.main.feels_like);
-}
 
